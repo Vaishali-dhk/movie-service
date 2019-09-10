9ae7164584e2b7254fac046a8cb3f7e5f79fc4f1
Prashant Kadatare <v-pkadatare@shutterfly.com>
2019-09-10 16:41:39 +0530

    Improvements

--- movie-service/src/main/java/com/practice/imdc/movieservice/controller/MovieController.java
+++ movie-service/src/main/java/com/practice/imdc/movieservice/controller/MovieController.java
@@ -1,17 +1,41 @@
 package com.practice.imdc.movieservice.controller;
 
 
+import static com.practice.imdc.movieservice.controller.MovieController.BASE_URL;
+
 import com.practice.imdc.movieservice.model.Movie;
+import com.practice.imdc.movieservice.service.MovieService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(BASE_URL)
 public class MovieController {
 
+  static final String BASE_URL = "/api/service/v1/movie";
 
-  @RequestMapping("/getAllMovies")
-  public Movie getAllMovies(){
-    return new Movie(1, "Spider-Man");
+  @Autowired
+  MovieService movieService;
+
+
+  @RequestMapping("/getAll")
+  public List<Movie> getAllMovies() {
+    return movieService.getAllMovies();
   }
+
+  @PostMapping("/movie")
+  public Movie add(@RequestBody Movie movie){
+    return movieService.save(movie);
+  }
+
+ /* @RequestMapping("/get/{id}")
+  public Movie getById(@RequestParam("id") int id) {
+    Objects.nonNull(id);
+    return movieService.getMovieById(id);
+  }*/
 
 }
--- movie-service/src/main/java/com/practice/imdc/movieservice/model/Movie.java
+++ movie-service/src/main/java/com/practice/imdc/movieservice/model/Movie.java
@@ -1,33 +1,26 @@
 package com.practice.imdc.movieservice.model;
 
 
+import com.practice.imdc.movieservice.constants.Genre;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 
   private int id;
   private String name;
+  private LocalDate releaseDate;
+  private String directorName;
+  private Genre genre;
+  private double rating;
 
-  public Movie(){
 
-  }
 
-  public Movie(int id, String name){
-    this.id = id;
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
 }
